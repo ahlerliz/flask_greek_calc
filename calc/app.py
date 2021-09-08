0a6ff58296bf3e0ef8eada19e7a9d1e3b37eb972
Colin Sidberry <colinsidberry@Colins-MacBook-Pro.local>
2021-09-08 12:34:00 -0700

    finished calc.py

--- calc/app.py
+++ calc/app.py
@@ -1,18 +1,37 @@
 # Put your app in her
-from flask import Flask
+from flask import Flask, request
 import operations
 
 app = Flask(__name__)
 
-@app.get("/add")
+#Question: what is wrong
+# @app.get("/add?a=<int:numA>&b=<int:numB>") #'/add?a=2&b=2')
+# def add(a,b):
+#     a =  request.args["a"]
+#     b = request.args["b"]
+#     return operations.add(a, b)
 
+@app.get("/add") #'/add?a=2&b=2')
+def add():
+    a =  int(request.args["a"])
+    b = int(request.args["b"])
+    return str(operations.add(a, b))
 
 @app.get("/sub")
+def sub():
+    a =  int(request.args["a"])
+    b = int(request.args["b"])
+    return str(operations.sub(a, b))
 
 
 @app.get("/mult")
+def mult():
+    a =  int(request.args["a"])
+    b = int(request.args["b"])
+    return str(operations.mult(a, b))
 
-@app.get("/divide")
-
-
-
+@app.get("/div")
+def div():
+    a =  int(request.args["a"])
+    b = int(request.args["b"])
+    return str(operations.div(a, b))
